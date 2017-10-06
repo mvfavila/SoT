69dda8383e5a6776b4153012adf3317b9bb68595
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-06 12:34:18 +1000

    Ninject added to UI.MVC layer. Ninject should be replaced later for a better DI tool.

---
+++ VS2017/SoT/src/SoT.Infra,CrossCutting.IoC/Ninject.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.Modules;
+
+namespace SoT.Infra_CrossCutting.IoC
+{
+    public class Ninject : NinjectModule
+    {
+        public override void Load()
+        {
+            // application
+
+            // service
+
+            // data repository
+
+            // data configuration
+
+            //Sample:
+            // Bind<ISampleRepository>().To<SampleRepository>();
+            throw new System.NotImplementedException();
+        }
+    }
+}
