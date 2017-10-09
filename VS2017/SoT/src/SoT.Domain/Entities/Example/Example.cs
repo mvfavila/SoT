721667f4ac3dcd2cef97dd911d0a1fcca3b6dede
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-09 16:24:38 +1000

    FIX: Ninject version in IoC project was different from the one in MVC project.

---
+++ VS2017/SoT/src/SoT.Domain/Entities/Example/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SoT.Domain.Entities.Example
+{
+    public class Example
+    {
+        public Example()
+        {
+            ExampleId = Guid.NewGuid();
+        }
+
+        public Guid ExampleId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
