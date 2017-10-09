10504561377e192f6da085084ca86147cd26d38b
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-09 19:32:48 +1000

    BaseRepository added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using SoT.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SoT.Infra.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
