e811da8513b7cbf4d5f0a9f93af11f928e422964
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-11 17:24:18 +1000

    WebAPI methods implemented.

---
+++ VS2017/SoT/src/SoT.Services.WebAPI/Controllers/ExamplesController.cs
@@ -0,0 +1,45 @@
+﻿using SoT.Application.AppServices;
+using SoT.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SoT.Services.WebAPI.Controllers
+{
+    public class ExamplesController : ApiController
+    {
+        private ExampleAppService exampleAppService = new ExampleAppService();
+
+        // GET: api/Examples
+        public IEnumerable<ExampleViewModel> Get()
+        {
+            return exampleAppService.GetAll();
+        }
+
+        // GET: api/Examples/5
+        public ExampleViewModel Get(Guid id)
+        {
+            return exampleAppService.GetById(id);
+        }
+
+        // POST: api/Examples
+        [HttpPost]
+        public void Post([FromBody]ExampleSubExampleViewModel exampleSubExampleViewModel)
+        {
+            exampleAppService.Add(exampleSubExampleViewModel);
+        }
+
+        // PUT: api/Examples/5
+        [HttpPut]
+        public void Put([FromBody]ExampleViewModel exampleViewModel)
+        {
+            exampleAppService.Update(exampleViewModel);
+        }
+
+        // DELETE: api/Examples/5
+        public void Delete(Guid id)
+        {
+            exampleAppService.Delete(id);
+        }
+    }
+}
