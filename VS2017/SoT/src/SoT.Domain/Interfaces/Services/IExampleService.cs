466a17702b594dc39841a543741b110b1b2084f2
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-11 18:16:38 +1000

    Example Domain Service added.

---
+++ VS2017/SoT/src/SoT.Domain/Interfaces/Services/IExampleService.cs
@@ -0,0 +1,24 @@
+﻿using SoT.Domain.Entities.Example;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SoT.Domain.Interfaces.Services
+{
+    public interface IExampleService : IDisposable
+    {
+        IEnumerable<Example> GetActive();
+
+        IEnumerable<Example> GetAll();
+
+        IEnumerable<Example> Find(Expression<Func<Example, bool>> predicate);
+
+        Example GetById(Guid id);
+
+        void Add(Example example);
+
+        void Update(Example example);
+
+        void Delete(Guid id);
+    }
+}
---
+++ VS2017/SoT/src/SoT.Domain/Services/ExampleService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SoT.Domain.Entities.Example;
+using SoT.Domain.Interfaces.Services;
+using SoT.Domain.Interfaces.Repository;
+
+namespace SoT.Domain.Services
+{
+    public class ExampleService : IExampleService
+    {
+        private readonly IExampleRepository exampleRepository;
+
+        public ExampleService(IExampleRepository exampleRepository)
+        {
+            this.exampleRepository = exampleRepository;
+        }
+
+        public void Add(Example example)
+        {
+            exampleRepository.Add(example);
+        }
+
+        public void Delete(Guid id)
+        {
+            exampleRepository.Delete(id);
+        }
+
+        public IEnumerable<Example> Find(Expression<Func<Example, bool>> predicate)
+        {
+            return exampleRepository.Find(predicate);
+        }
+
+        public IEnumerable<Example> GetActive()
+        {
+            return exampleRepository.GetActive();
+        }
+
+        public IEnumerable<Example> GetAll()
+        {
+            return exampleRepository.GetAll();
+        }
+
+        public Example GetById(Guid id)
+        {
+            return exampleRepository.GetById(id);
+        }
+
+        public void Update(Example example)
+        {
+            exampleRepository.Update(example);
+        }
+
+        public void Dispose()
+        {
+            exampleRepository.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
