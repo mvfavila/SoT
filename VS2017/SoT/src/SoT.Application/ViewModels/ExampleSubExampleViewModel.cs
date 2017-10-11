45d37da953ca3a94a3cc5b0e8129b83d82f265ee
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-11 15:20:36 +1000

    Annotation examples added.

--- VS2017/SoT/src/SoT.Application/ViewModels/ExampleSubExampleViewModel.cs
+++ VS2017/SoT/src/SoT.Application/ViewModels/ExampleSubExampleViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SoT.Application.ViewModels
 {
@@ -13,16 +14,33 @@ namespace SoT.Application.ViewModels
         // Example
         public Guid ExampleId { get; set; }
 
+        [Required(ErrorMessage = "Field name is required")]
+        [MaxLength(150, ErrorMessage = "The maximum length of the field Name is 150")]
         public string Name { get; set; }
 
+        [Display(Name = "Example Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage = "Date format invalid")]
         public DateTime DatePropertyName { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Active { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime RegisterDate { get; set; }
 
         // SubExample
 
         public Guid SubExampleId { get; set; }
 
+        [Required(ErrorMessage = "Field E-mail is required")]
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "E-mail format invalid")]
         public string StringPropertyName { get; set; }
 
+        [Display(Name = "Example Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage = "Date format invalid")]
         public DateTime SubExampleDatePropertyName { get; set; }
 
         public int CalculatedPropertyName
