c1fda4a7506c55b5118310fe2e9b61f1fda8d0b4
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-11 17:31:35 +1000

    Config changed so that the WebAPI returns JSON responses.

--- VS2017/SoT/src/SoT.Services.WebAPI/App_Start/WebApiConfig.cs
+++ VS2017/SoT/src/SoT.Services.WebAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,10 @@ namespace SoT.Services.WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes
+                .FirstOrDefault(t => t.MediaType == "applicatio/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
