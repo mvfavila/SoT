3f177c355132ac160dc416dcc06fb62d189deb0e
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-12 12:41:01 +1000

    Unit of Work manager implemented.

--- VS2017/SoT/src/SoT.Infra.Data/UoW/UnitOfWork.cs
+++ VS2017/SoT/src/SoT.Infra.Data/UoW/UnitOfWork.cs
@@ -1,21 +1,48 @@
-﻿using SoT.Infra.Data.Context;
+﻿using CommonServiceLocator;
+using SoT.Infra.Data.Context;
 using SoT.Infra.Data.Interfaces;
+using System;
 
 namespace SoT.Infra.Data.UoW
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly SoTContext dbContext;
-        private readonly ContextManager contextManager;
+        private readonly ContextManager contextManager =
+            ServiceLocator.Current.GetInstance<IContextManager>() as ContextManager;
+        private bool disposed;
+
+        public UnitOfWork()
+        {
+            dbContext = contextManager.GetContext();
+        }
 
         public void BeginTransaction()
         {
-            throw new System.NotImplementedException();
+            disposed = false;
         }
 
         public void SaveChanges()
         {
-            throw new System.NotImplementedException();
+            dbContext.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    dbContext.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
