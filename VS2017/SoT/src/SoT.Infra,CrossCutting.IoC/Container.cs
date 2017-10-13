1d0e0672fec7e90330bc4c6f5adc3f1fad53637d
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-13 16:27:46 +1000

    Ninject removed from application as the DI provider.

--- VS2017/SoT/src/SoT.Infra,CrossCutting.IoC/Container.cs
+++ VS2017/SoT/src/SoT.Infra,CrossCutting.IoC/Container.cs
@@ -1,14 +1,6 @@
-﻿using Ninject;
-
-namespace SoT.Infra.CrossCutting.IoC
+﻿namespace SoT.Infra.CrossCutting.IoC
 {
     public class Container
     {
-        public StandardKernel GetModule()
-        {
-            return new StandardKernel(
-                new Ninject()
-                );
-        }
     }
 }
