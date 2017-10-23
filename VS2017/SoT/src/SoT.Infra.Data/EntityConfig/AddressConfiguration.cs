85932a55c133786cc258f3a64d7bfdb3161c5bc8
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-23 16:57:36 +1000

    AddressConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/AddressConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class AddressConfiguration : EntityTypeConfiguration<Address>
+    {
+        public AddressConfiguration()
+        {
+            HasKey(a => a.AddressId);
+
+            Property(a => a.Street01)
+                .HasMaxLength(300)
+                .IsRequired();
+
+            Property(a => a.Complement)
+                .HasMaxLength(300)
+                .IsOptional();
+
+            Property(a => a.Postcode)
+                .HasMaxLength(30)
+                .IsOptional();
+
+            Property(a => a.AdventureId)
+                .IsRequired();
+
+            HasRequired(s => s.Adventure)
+                .WithMany()
+                .HasForeignKey(s => s.AdventureId);
+
+            // TODO: add validation and uncomment the line below
+            //Ignore(s => s.ValidationResult);
+        }
+    }
+}
