1c9429bd0c22badf9d1498152d7244c0eca776f2
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-27 20:49:04 +1000

    UserAdminController and related ViewModel added.

---
+++ VS2017/SoT/src/SoT.Presentation.UI.MVC/ViewModels/User/EditUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SoT.Presentation.UI.MVC.ViewModels.User
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
+    }
+}
