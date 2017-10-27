85388a073126d2c58c8000b218180de0029742bb
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-10-27 20:40:00 +1000

    RolesAdminController and related View added.

---
+++ VS2017/SoT/src/SoT.Presentation.UI.MVC/ViewModels/Roles/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoT.Presentation.UI.MVC.ViewModels.Roles
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Role name")]
+        public string Name { get; set; }
+    }
+}
