271b44dae809ff8d8e7686f63c56bb40cca02b5a
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 17:09:31 +1000

    ProviderConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/ProviderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class ProviderConfiguration : EntityTypeConfiguration<Provider>
+    {
+        public ProviderConfiguration()
+        {
+            HasKey(p => p.ProviderId);
+
+            Property(p => p.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            Property(p => p.Name)
+                .HasMaxLength(400)
+                .IsRequired();
+
+            Property(p => p.CompanyName)
+                .HasMaxLength(400)
+                .IsRequired();
+
+            Property(p => p.Active)
+                .IsRequired();
+
+            Ignore(p => p.ValidationResult);
+        }
+    }
+}
