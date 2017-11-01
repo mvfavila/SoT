1465611d4b13426b45326ae27333a1fa5d7ac003
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 16:21:26 +1000

    RegionConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/RegionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class RegionConfiguration : EntityTypeConfiguration<Region>
+    {
+        public RegionConfiguration()
+        {
+            HasKey(r => r.RegionId);
+
+            Property(r => r.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            Property(r => r.Active)
+                .IsRequired();
+
+            HasRequired(r => r.Continent)
+                .WithMany()
+                .HasForeignKey(r => r.ContinentId);
+
+            Ignore(r => r.ValidationResult);
+        }
+    }
+}
