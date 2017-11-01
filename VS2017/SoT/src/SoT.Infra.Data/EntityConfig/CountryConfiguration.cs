6c11c31b814f7c725334abc5879717f47726c622
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 16:28:00 +1000

    CountryConfiguration added

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/CountryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class CountryConfiguration : EntityTypeConfiguration<Country>
+    {
+        public CountryConfiguration()
+        {
+            HasKey(c => c.CountryId);
+
+            Property(c => c.Name)
+                .HasMaxLength(70)
+                .IsRequired();
+
+            Property(c => c.Active)
+                .IsRequired();
+
+            HasRequired(c => c.Region)
+                .WithMany()
+                .HasForeignKey(c => c.RegionId);
+
+            Ignore(c => c.ValidationResult);
+        }
+    }
+}
