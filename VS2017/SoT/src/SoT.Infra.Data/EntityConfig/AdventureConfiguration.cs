fb81d0c59b0fd88f1a510390079851c9d787f2df
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 17:01:11 +1000

    AdventureConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/AdventureConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class AdventureConfiguration : EntityTypeConfiguration<Adventure>
+    {
+        public AdventureConfiguration()
+        {
+            HasKey(a => a.AdventureId);
+
+            Property(a => a.Name)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            Property(a => a.Active)
+                .IsRequired();
+
+            HasRequired(a => a.Category)
+                .WithMany()
+                .HasForeignKey(a => a.CategoryId);
+
+            HasRequired(a => a.City)
+                .WithMany()
+                .HasForeignKey(a => a.CityId);
+
+            HasRequired(a => a.Address)
+                .WithMany()
+                .HasForeignKey(a => a.AddressId);
+
+            Property(a => a.InsurenceMinimalAmount)
+                .IsOptional()
+                .HasPrecision(9, 2);
+
+            HasRequired(a => a.Provider)
+                .WithMany()
+                .HasForeignKey(a => a.ProviderId);
+
+            Ignore(a => a.ValidationResult);
+        }
+    }
+}
