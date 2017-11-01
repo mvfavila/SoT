153fe1fcabeb2a72d58a64d17437b461d8c12f14
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 16:14:25 +1000

    ContinentConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/ContinentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class ContinentConfiguration : EntityTypeConfiguration<Continent>
+    {
+        public ContinentConfiguration()
+        {
+            HasKey(c => c.ContinentId);
+
+            Property(c => c.Name)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            Property(c => c.Active)
+                .IsRequired();
+        }
+    }
+}
