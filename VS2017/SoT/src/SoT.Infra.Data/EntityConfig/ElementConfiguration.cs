17bc5cc52c0f358620ab9132012c729452c611e0
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-01 15:47:51 +1000

    ElementConfiguration added.

---
+++ VS2017/SoT/src/SoT.Infra.Data/EntityConfig/ElementConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using SoT.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SoT.Infra.Data.EntityConfig
+{
+    public class ElementConfiguration : EntityTypeConfiguration<Element>
+    {
+        public ElementConfiguration()
+        {
+            HasKey(e => e.ElementId);
+
+            Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            Property(e => e.Active)
+                .IsRequired();
+        }
+    }
+}
