2aacea880a701502cc9c50080fd1169a4a17a82d
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-16 15:12:33 +1000

    ICountryService added and implemented.

---
+++ VS2017/SoT/src/SoT.Domain/Interfaces/Services/ICountryService.cs
@@ -0,0 +1,12 @@
+﻿using SoT.Domain.Entities;
+using SoT.Domain.ValueObjects;
+
+namespace SoT.Domain.Interfaces.Services
+{
+    public interface ICountryService : IBaseService<Country>
+    {
+        new ValidationResult Add(Country country);
+
+        new ValidationResult Update(Country country);
+    }
+}
---
+++ VS2017/SoT/src/SoT.Domain/Services/CountryService.cs
@@ -0,0 +1,29 @@
+﻿using SoT.Domain.Entities;
+using SoT.Domain.Interfaces.Repository.ReadOnly;
+using SoT.Domain.Interfaces.Services;
+using SoT.Domain.ValueObjects;
+using System;
+
+namespace SoT.Domain.Services
+{
+    public class CountryService : BaseService<Country>, ICountryService
+    {
+        private readonly ICountryReadOnlyRepository countryReadOnlyRepository;
+
+        public CountryService(ICountryReadOnlyRepository countryReadOnlyRepository)
+            : base(null, countryReadOnlyRepository)
+        {
+            this.countryReadOnlyRepository = countryReadOnlyRepository;
+        }
+
+        ValidationResult ICountryService.Add(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        ValidationResult ICountryService.Update(Country country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
