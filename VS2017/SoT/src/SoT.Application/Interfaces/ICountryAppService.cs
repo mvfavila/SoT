44d1205ae55fd8f2ec070a453d6222373939d3c4
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-16 15:31:29 +1000

    ICountryAppService and CountryViewModel added.

---
+++ VS2017/SoT/src/SoT.Application/Interfaces/ICountryAppService.cs
@@ -0,0 +1,15 @@
+﻿using SoT.Application.Validation;
+using SoT.Application.ViewModels;
+using System;
+
+namespace SoT.Application.Interfaces
+{
+    public interface ICountryAppService : IDisposable
+    {
+        CountryViewModel GetById(Guid id);
+
+        ValidationAppResult Add(CountryViewModel countryViewModel);
+
+        ValidationAppResult Update(CountryViewModel countryViewModel);
+    }
+}
---
+++ VS2017/SoT/src/SoT.Application/ViewModels/CountryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoT.Application.ViewModels
+{
+    public class CountryViewModel
+    {
+        public CountryViewModel()
+        {
+            CountryId = Guid.NewGuid();
+        }
+
+        [ScaffoldColumn(false)]
+        public Guid CountryId { get; set; }
+
+        [Required(ErrorMessage = "Field Name is required")]
+        [MaxLength(70, ErrorMessage = "The maximum length of the field Name is 70")]
+        public string Name { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Active { get; set; }
+
+        // TODO: commented only so an example implementation could be created for a Wep API class
+        //public Guid RegionId { get; set; }
+
+        //public virtual Region Region { get; set; }
+
+        //public virtual IEnumerable<City> Cities { get; set; }
+    }
+}
