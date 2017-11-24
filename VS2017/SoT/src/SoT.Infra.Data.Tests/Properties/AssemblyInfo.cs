708ae72a3e227b2953272bbf376fd29538518b74
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-11-24 15:04:42 +1000

    SoT.Infra.Data.Tests project added.

---
+++ VS2017/SoT/src/SoT.Infra.Data.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoT.Infra.Data.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoT.Infra.Data.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13e426c5-b2ee-4ee8-a921-9677d32b8727")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
