86f1deb065dba8e9592611880e26a008f9b8fad5
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 22:05:36 +1000

    CityAppServiceTest.City_GetAll_Sucess() added.

--- VS2017/SoT/src/SoT.Application.Tests/AppServices/CityAppServiceTest.cs
+++ VS2017/SoT/src/SoT.Application.Tests/AppServices/CityAppServiceTest.cs
@@ -25,6 +25,41 @@ namespace SoT.Application.Tests.AppServices
                 );
         }
 
+        [Fact(DisplayName = "Get all Cities")]
+        [Trait(nameof(City), "App Service")]
+        public void City_GetAll_Sucess()
+        {
+            // Arrange
+            var country = new Faker<Country>()
+                .CustomInstantiator(c => Country.FactoryTest(
+                    Guid.NewGuid(),
+                    c.Address.Country(),
+                    c.Random.Bool(),
+                    Guid.NewGuid(),
+                    null)).Generate();
+
+            var cityFaker = new Faker<City>()
+                .CustomInstantiator(c => City.FactoryTest(
+                    Guid.NewGuid(),
+                    c.Address.City(),
+                    true,
+                    country.CountryId,
+                    country));
+
+            mocker.Create<CityAppService>();
+            var cityAppService = mocker.Resolve<CityAppService>();
+            var cityService = mocker.GetMock<ICityService>();
+            cityService
+                .Setup(c => c.GetAll())
+                .Returns(cityFaker.Generate(50000));
+
+            // Act
+            cityAppService.GetAll();
+
+            // Assert
+            cityService.Verify(c => c.GetAll(), Times.Once());
+        }
+
         [Fact(DisplayName = "Get active Cities by Country Id")]
         [Trait(nameof(City), "App Service")]
         public void City_GetActiveByCountry_Sucess()
