5a3fbad84e8466091cc15825eb4b959bf2a1f194
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 10:48:40 +1000

    Region Validation tests renamed to friendly names

--- VS2017/SoT/src/SoT.Domain.Tests/Validation/Region/RegionIsVerifiedForRegistration.cs
+++ VS2017/SoT/src/SoT.Domain.Tests/Validation/Region/RegionIsVerifiedForRegistration.cs
@@ -6,7 +6,8 @@ namespace SoT.Domain.Tests.Validation.Region
 {
     public class RegionIsVerifiedForRegistration
     {
-        [Fact]
+        [Fact(DisplayName = "Valid instance")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_MustBeValid()
         {
             var region = Domain.Entities.Region.FactoryTest(
@@ -23,7 +24,27 @@ namespace SoT.Domain.Tests.Validation.Region
             Assert.False(region.ValidationResult.Errors.Any());
         }
 
-        [Fact]
+        [Fact(DisplayName = "Id is required")]
+        [Trait(nameof(Region), "Instantiation")]
+        public void Region_Instantiate_KeyMustNotBeNull()
+        {
+            var region = Domain.Entities.Region.FactoryTest(
+                TestConstants.REGION_ID_INVALID,
+                TestConstants.REGION_NAME_VALID,
+                TestConstants.ACTIVE,
+                TestConstants.CONTINENT_ID_VALID,
+                TestConstants.CONTINENT_VALID
+                );
+
+            var isValid = region.IsValid();
+
+            Assert.False(isValid);
+            Assert.Contains($"{nameof(Domain.Entities.Region.RegionId)} is required",
+                region.ValidationResult.Errors.Select(error => error.Message).ToList());
+        }
+
+        [Fact(DisplayName = "Name is required (Not null)")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_NameMustNotBeNull()
         {
             var region = Domain.Entities.Region.FactoryTest(
@@ -41,7 +62,8 @@ namespace SoT.Domain.Tests.Validation.Region
                 region.ValidationResult.Errors.Select(error => error.Message).ToList());
         }
 
-        [Fact]
+        [Fact(DisplayName = "Name is required (Not empty)")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_NameMustNotBeEmpty()
         {
             var region = Domain.Entities.Region.FactoryTest(
@@ -59,7 +81,8 @@ namespace SoT.Domain.Tests.Validation.Region
                 region.ValidationResult.Errors.Select(error => error.Message).ToList());
         }
 
-        [Fact]
+        [Fact(DisplayName = "Name is required (Not empty spaces)")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_NameMustNotBeEmptySpaces()
         {
             var region = Domain.Entities.Region.FactoryTest(
@@ -77,7 +100,8 @@ namespace SoT.Domain.Tests.Validation.Region
                 region.ValidationResult.Errors.Select(error => error.Message).ToList());
         }
 
-        [Fact]
+        [Fact(DisplayName = "Name must have 100 chars or less")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_NameMustHaveValidLength()
         {
             var region = Domain.Entities.Region.FactoryTest(
@@ -107,7 +131,8 @@ namespace SoT.Domain.Tests.Validation.Region
                 region.ValidationResult.Errors.Select(error => error.Message).ToList());
         }
 
-        [Fact]
+        [Fact(DisplayName = "Continent Id is required")]
+        [Trait(nameof(Region), "Instantiation")]
         public void Region_Instantiate_ContinentMustNotBeNull()
         {
             var region = Domain.Entities.Region.FactoryTest(
