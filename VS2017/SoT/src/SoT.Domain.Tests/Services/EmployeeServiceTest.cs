8bd111405a8a21f133396155d9718ea840c7f631
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 14:15:48 +1000

    EmployeeServiceTest.Employee_Update_Sucess() added.

--- VS2017/SoT/src/SoT.Domain.Tests/Services/EmployeeServiceTest.cs
+++ VS2017/SoT/src/SoT.Domain.Tests/Services/EmployeeServiceTest.cs
@@ -57,5 +57,44 @@ namespace SoT.Domain.Tests.Services
             Assert.True(validationResult.IsValid);
             Assert.Empty(validationResult.Errors);
         }
+
+        [Fact(DisplayName = "Update Employee Sucess")]
+        [Trait(nameof(Employee), "Domain Service")]
+        public void Employee_Update_Sucess()
+        {
+            // Arrange
+            mocker.Create<EmployeeService>();
+
+            var employeeService = mocker.Resolve<EmployeeService>();
+            var employeeRepository = mocker.GetMock<IEmployeeRepository>();
+
+            var providerFaker = new Faker<Provider>()
+                .CustomInstantiator(p => Provider.FactoryTest(
+                    Guid.NewGuid(),
+                    p.Company.CompanyName(),
+                    new List<Adventure>(),
+                    new List<Employee>(),
+                    true
+                    ));
+
+            var employeeFaker = new Faker<Employee>()
+                .CustomInstantiator(e => Employee.FactoryTest(
+                    Guid.NewGuid(),
+                    e.Date.Past(90, DateTime.Now.AddYears(-18)),
+                    Guid.NewGuid(),
+                    providerFaker.Generate(),
+                    Guid.NewGuid()
+                    ));
+
+            var employee = employeeFaker.Generate();
+
+            // Act
+            var validationResult = employeeService.Update(employee);
+
+            // Assert
+            employeeRepository.Verify(e => e.Update(employee), Times.Once());
+            Assert.True(validationResult.IsValid);
+            Assert.Empty(validationResult.Errors);
+        }
     }
 }
