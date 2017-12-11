57891187784f5615247068aeb7d3fbda1f5a6684
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 13:08:51 +1000

    CountryServiceTest added.

---
+++ VS2017/SoT/src/SoT.Domain.Tests/Services/CountryServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoMoq;
+using Moq;
+using SoT.Domain.Interfaces.Repository.ReadOnly;
+using SoT.Domain.Services;
+using Xunit;
+
+namespace SoT.Domain.Tests.Services
+{
+    public class CountryServiceTest
+    {
+        private readonly AutoMoqer mocker;
+
+        public CountryServiceTest()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Fact(DisplayName = "Get All Active Countries")]
+        [Trait("Country", "Domain Service")]
+        public void Country_GetAllActive_Sucess()
+        {
+            // Arrange
+            mocker.Create<CountryService>();
+
+            var countryService = mocker.Resolve<CountryService>();
+            var countryRepository = mocker.GetMock<ICountryReadOnlyRepository>();
+
+            // Act
+            countryService.GetAllActive();
+
+            // Assert
+            countryRepository.Verify(c => c.GetAllBySituation(It.Is<bool>(s => s)), Times.Once());
+        }
+    }
+}
