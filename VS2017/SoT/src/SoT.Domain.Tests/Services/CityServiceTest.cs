c1755a0eebb566dc2d361d17c4391e24b0d4dc42
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 12:58:53 +1000

    CityServiceTest added.

---
+++ VS2017/SoT/src/SoT.Domain.Tests/Services/CityServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoMoq;
+using Moq;
+using SoT.Domain.Interfaces.Repository.ReadOnly;
+using SoT.Domain.Services;
+using SoT.Domain.Tests.Shared;
+using System;
+using Xunit;
+
+namespace SoT.Domain.Tests.Services
+{
+    public class CityServiceTest
+    {
+        private readonly AutoMoqer mocker;
+
+        public CityServiceTest()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Fact(DisplayName = "Get Active Cities by Country Id")]
+        [Trait("City", "Domain Service")]
+        public void City_GetActiveByCountry_Sucess()
+        {
+            // Arrange
+            mocker.Create<CityService>();
+
+            var cityService = mocker.Resolve<CityService>();
+            var cityRepository = mocker.GetMock<ICityReadOnlyRepository>();
+            var countryId = Guid.NewGuid();
+
+            // Act
+            cityService.GetActiveByCountry(countryId);
+
+            // Assert
+            cityRepository.Verify(a => a.GetActiveByCountry(It.IsAny<Guid>()), Times.Once());
+        }
+    }
+}
