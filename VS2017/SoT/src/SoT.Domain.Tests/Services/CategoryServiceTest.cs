6f0951fb8bb8e9b9a44000c93afe6f5f2cbdfe79
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-11 12:53:14 +1000

    CategoryServiceTest added.

---
+++ VS2017/SoT/src/SoT.Domain.Tests/Services/CategoryServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoMoq;
+using Moq;
+using SoT.Domain.Interfaces.Repository.ReadOnly;
+using SoT.Domain.Services;
+using Xunit;
+
+namespace SoT.Domain.Tests.Services
+{
+    public class CategoryServiceTest
+    {
+        private readonly AutoMoqer mocker;
+
+        public CategoryServiceTest()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Fact(DisplayName = "Get All Active Categories")]
+        [Trait("Category", "Domain Service")]
+        public void Category_GetAllActive_Sucess()
+        {
+            // Arrange
+            mocker.Create<CategoryService>();
+
+            var categoryService = mocker.Resolve<CategoryService>();
+            var categoryRepository = mocker.GetMock<ICategoryReadOnlyRepository>();
+
+            // Act
+            categoryService.GetAllActive();
+
+            // Assert
+            categoryRepository.Verify(a => a.GetAllBySituation(It.Is<bool>(s => s)), Times.Once());
+        }
+    }
+}
