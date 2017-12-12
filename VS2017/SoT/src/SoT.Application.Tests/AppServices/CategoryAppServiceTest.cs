2bf55ac2702a2faeb0cb86c3f2aa8e79d9c0b062
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-12 14:15:35 +1000

    CategoryAppServiceTest.Category_GetAllActive_Sucess() tests Mapper (internal method)

--- VS2017/SoT/src/SoT.Application.Tests/AppServices/CategoryAppServiceTest.cs
+++ VS2017/SoT/src/SoT.Application.Tests/AppServices/CategoryAppServiceTest.cs
@@ -6,6 +6,7 @@ using SoT.Domain.Entities;
 using SoT.Domain.Interfaces.Services;
 using System;
 using System.IO;
+using System.Linq;
 using System.Web;
 using Xunit;
 
@@ -30,26 +31,33 @@ namespace SoT.Application.Tests.AppServices
         public void Category_GetAllActive_Sucess()
         {
             // Arrange
-            var categoryFaker = new Faker<Category>()
+            var categories = new Faker<Category>()
                 .CustomInstantiator(c => Category.FactoryTest(
                     Guid.NewGuid(),
                     c.Commerce.Categories(1)[0],
                     true,
                     Guid.NewGuid(),
-                    null));
+                    null)).Generate(100);
 
             mocker.Create<CategoryAppService>();
             var categoryAppService = mocker.Resolve<CategoryAppService>();
             var categoryService = mocker.GetMock<ICategoryService>();
             categoryService
                 .Setup(c => c.GetAllActive())
-                .Returns(categoryFaker.Generate(100));
+                .Returns(categories);
 
             // Act
-            categoryAppService.GetAllActive();
+            var categoryViewModels = categoryAppService.GetAllActive().ToList();
 
             // Assert
             categoryService.Verify(c => c.GetAllActive(), Times.Once());
+            for (int i = 0; i < categories.Count; i++)
+            {
+                Assert.Equal(categories[i].CategoryId, categoryViewModels[i].CategoryId);
+                Assert.Equal(categories[i].Name, categoryViewModels[i].Name);
+                Assert.Equal(categories[i].Active, categoryViewModels[i].Active);
+                Assert.Equal(categories[i].ElementId, categoryViewModels[i].ElementId);
+            }
         }
     }
 }
