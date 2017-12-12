2047f47b83216be3b7df0d8374d0ad6783195e44
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2017-12-12 14:20:53 +1000

    CityAppServiceTest.City_GetAll_Sucess() tests Mapper (internal method)

--- VS2017/SoT/src/SoT.Application.Tests/AppServices/CityAppServiceTest.cs
+++ VS2017/SoT/src/SoT.Application.Tests/AppServices/CityAppServiceTest.cs
@@ -6,6 +6,7 @@ using SoT.Domain.Entities;
 using SoT.Domain.Interfaces.Services;
 using System;
 using System.IO;
+using System.Linq;
 using System.Web;
 using Xunit;
 
@@ -30,34 +31,33 @@ namespace SoT.Application.Tests.AppServices
         public void City_GetAll_Sucess()
         {
             // Arrange
-            var country = new Faker<Country>()
-                .CustomInstantiator(c => Country.FactoryTest(
-                    Guid.NewGuid(),
-                    c.Address.Country(),
-                    c.Random.Bool(),
-                    Guid.NewGuid(),
-                    null)).Generate();
-
-            var cityFaker = new Faker<City>()
+            var cities = new Faker<City>()
                 .CustomInstantiator(c => City.FactoryTest(
                     Guid.NewGuid(),
                     c.Address.City(),
                     true,
-                    country.CountryId,
-                    country));
+                    Guid.NewGuid(),
+                    null)).Generate(50000);
 
             mocker.Create<CityAppService>();
             var cityAppService = mocker.Resolve<CityAppService>();
             var cityService = mocker.GetMock<ICityService>();
             cityService
                 .Setup(c => c.GetAll())
-                .Returns(cityFaker.Generate(50000));
+                .Returns(cities);
 
             // Act
-            cityAppService.GetAll();
+            var cityViewModels = cityAppService.GetAll().ToList();
 
             // Assert
             cityService.Verify(c => c.GetAll(), Times.Once());
+            for (int i = 0; i < cities.Count; i++)
+            {
+                Assert.Equal(cities[i].CityId, cityViewModels[i].CityId);
+                Assert.Equal(cities[i].Name, cityViewModels[i].Name);
+                Assert.Equal(cities[i].Active, cityViewModels[i].Active);
+                Assert.Equal(cities[i].CountryId, cityViewModels[i].CountryId);
+            }
         }
 
         [Fact(DisplayName = "Get active Cities by Country Id")]
